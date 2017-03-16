a230c69b6f6185a5e47fe1f59b63e6aaf9795d72
Douglas Ashton <douglas.j.ashton@gmail.com>
2017-03-16 21:06:48 +0000

    solr hostname. Add a document test

--- tests/testthat/test-update.R
+++ tests/testthat/test-update.R
@@ -2,7 +2,11 @@ context("Updating Documents")
 
 test_that("Add a document", {
 
-  if(!check_solr_instance("awe")) skip("Needs solr instance")
+  if(!check_solr_instance("http://solr")) skip("Needs solr instance")
 
-  expect_true(FALSE)
+  nb <- readRDS("notebooks/notebook01.rds")
+
+  response <- update_solr(nb, 0)
+  
+  # TODO Check it worked!
 })
