e3f40a653a89def26fccc6024f600176b5e52229
Douglas Ashton <douglas.j.ashton@gmail.com>
2017-03-16 18:32:15 +0000

    Add solr instance check function

---
+++ tests/testthat/helper-check-solr.R
@@ -0,0 +1,16 @@
+
+check_solr_instance <- function(hostname = "http://localhost",
+                                port = 8983,
+                                path = "solr/rcloudnotebooks/admin/ping",
+                                ...) {
+  url <- httr::parse_url(hostname)
+  url$port = port
+  url$path <- paste(url$path, path, sep = "/")
+  url <- c(url, list(...))
+  class(url) <- "url"
+
+  response <- tryCatch(httr::GET(url, timeout(1)),
+                       error = function(e) list(status_code=-1))
+
+  isTRUE(response$status_code == 200)
+}
---
+++ tests/testthat/test-update.R
@@ -0,0 +1,8 @@
+context("Updating Documents")
+
+test_that("Add a document", {
+
+  if(!check_solr_instance("awe")) skip("Needs solr instance")
+
+  expect_true(FALSE)
+})
