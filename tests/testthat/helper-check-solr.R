242b6c8b2cde562e7f58671f53b30f8bc7a0d3c8
Douglas Ashton <douglas.j.ashton@gmail.com>
2017-08-01 16:35:43 +0100

    test basic search. Fixes #18

--- tests/testthat/helper-check-solr.R
+++ tests/testthat/helper-check-solr.R
@@ -1,13 +1,24 @@
 
-check_solr_instance <- function(hostname = "http://localhost",
-                                port = 8983,
-                                path = "solr/rcloudnotebooks/admin/ping",
-                                ...) {
+make_solr_url <- function(hostname = "http://localhost",
+                          port = 8983,
+                          path = "solr/rcloudnotebooks",
+                          query = NULL,
+                          ...) {
   url <- httr::parse_url(hostname)
   url$port = port
   url$path <- paste(url$path, path, sep = "/")
+  url$query <- query
   url <- c(url, list(...))
   class(url) <- "url"
+  url
+}
+
+check_solr_instance <- function(hostname = "http://localhost",
+                                port = 8983,
+                                path = "solr/rcloudnotebooks/admin/ping",
+                                query = NULL,
+                                ...) {
+  url <- make_solr_url(hostname = hostname, port = port, path = path, ...)
 
   response <- tryCatch(httr::GET(url, httr::timeout(1)),
                        error = function(e) list(status_code=-1))
---
+++ tests/testthat/test-search.R
@@ -0,0 +1,21 @@
+context("Search")
+
+if(check_solr_instance("http://solr")) {
+
+  url <- make_solr_url("http://solr", path = "solr/rcloudnotebooks/update", query = list(commit = "true"))
+
+  # Uploads a bunch of notebooks
+  httr::POST(url, body = httr::upload_file("notebooks/allnotebooks.json"))
+
+  on.exit({
+    httr::POST(url, httr::content_type_xml(), body = "<delete><query>*:*</query></delete>")
+  })
+}
+
+test_that("rcloud.search", {
+
+  res <- rcloud.search("hist", all_sources = FALSE, sortby = "starcount",
+                       orderby = "desc", start = 0, pagesize = 10, group.limit = 4)
+
+  expect_equal(res$matches, 14)
+})
