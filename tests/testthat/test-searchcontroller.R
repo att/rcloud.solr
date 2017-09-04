9827d8ba667f0fe4e1d3f2080c48f8e07953367f
Doug Ashton <douglas.j.ashton@gmail.com>
2017-09-04 14:09:19 +0100

    Fix and test global search instance

--- tests/testthat/test-searchcontroller.R
+++ tests/testthat/test-searchcontroller.R
@@ -8,3 +8,13 @@ test_that("Initialize", {
 
   expect_equal(config$solr.url, "http://example.com:8983")
 })
+
+test_that("Global search instance exists", {
+
+  expect_is(.SC, "SearchController")
+
+  expect_is(.SC, "R6")
+
+  expect_null(.SC$get_config())
+
+})
