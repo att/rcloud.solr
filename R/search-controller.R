3e78385e1f0eef2cbdd661d8fca1090727df3890
Doug Ashton <douglas.j.ashton@gmail.com>
2017-09-04 11:50:42 +0100

    scaffold initialize and docs for R6 class

--- R/search-controller.R
+++ R/search-controller.R
@@ -1,3 +1,36 @@
+
+#' Search Controller
+#'
+#' A description of this R6 class
+#'
+#' @section Details:
+#' \code{$new()} starts a new search controller
+#'
+#' @name SearchController
+#' @examples
+#' # Might need rcloud.support running for this to work
+#' \dontrun{
+#' SC <- SearchController$new(something)
+#' }
+#'
+NULL
+
+#' @export
+
 SearchController <- R6::R6Class("SearchController",
-  public = list(a = 1)
+  public = list(
+
+    initialize = function(searchConfig)
+      sc_initialize(self, private, searchConfig)
+  ),
+
+  private = list(
+    sources = NULL
+  )
 )
+
+sc_initialize <- function(self, private, searchConfig) {
+
+  private$sources <- searchConfig$sources
+
+}
