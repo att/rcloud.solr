394bf6a5e92350a28c637d5bf88769a5bf5926bc
dougmet <douglas.j.ashton@gmail.com>
2017-09-27 16:21:55 +0100

    fixes #46. Support sorting by time or character fields

--- tests/testthat/test-sort.R
+++ tests/testthat/test-sort.R
@@ -33,3 +33,44 @@ test_that("Sort by star count", {
   expect_true(all(running_greater_than))
 
 })
+
+
+test_that("Sort by updated_at", {
+
+  raw_results <- readRDS("raw_results.rds")
+
+  sorted_results <- sc_merge_results(raw_results, sortby = "updated_at", orderby = "desc")
+  sorted_times <- vapply(sorted_results$notebooks, `[[`, "updated_at", FUN.VALUE = character(1))
+
+  # Cross check against lubridate
+  running_less_than <- diff(lubridate::ymd_hms(sorted_times)) <= 0
+  expect_true(all(running_less_than))
+
+})
+
+test_that("Sort by description", {
+
+  raw_results <- readRDS("raw_results.rds")
+
+  sorted_results <- sc_merge_results(raw_results, sortby = "description", orderby = "asc")
+  sorted_descriptions <- vapply(sorted_results$notebooks, `[[`, "description", FUN.VALUE = character(1))
+
+  # Cross check against lubridate
+  expect_equal(sorted_descriptions[1], "Hist1")
+
+})
+
+
+
+test_that("Infer types", {
+
+  missing_numeric <- list(1,4,3,NULL,2)
+  out <- sc_infer_sort_col(missing_numeric)
+
+  expect_equal(out, c(1,4,3,NA,2))
+
+  mixed_types <- list(1,"A",3,NULL,2)
+  out <- sc_infer_sort_col(mixed_types)
+
+  expect_equal(out, c("1", "A", "3", NA, "2"))
+})
