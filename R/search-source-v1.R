06b1830c4e315728d9bb0ca17022a7b32f9a30ff
Doug Ashton <douglas.j.ashton@gmail.com>
2017-09-29 19:03:30 +0100

    add empty V1 class inherits SerachSource

---
+++ R/search-source-v1.R
@@ -0,0 +1,26 @@
+# Class functions generally redirect to internal functions beginning ss_*
+
+#' Search a Version 1 Source
+#'
+#' An R6 class to make requests to individual src_params sources. Child of \code{\link{SearchSource}}
+#'
+#' @name SearchSourceV1
+#' @examples
+#' \dontrun{
+#' SS <-
+#' SearchSourceV1$new(
+#'   src_params = list(source =  "main_source",
+#'                     solr.url = "http://solr:8983/solr/rcloudnotebooks")
+#' )
+#' }
+#'
+NULL
+
+#' @export
+
+SearchSourceV1 <- R6::R6Class(
+  "SearchSourceV1",
+
+  inherit = SearchSource,
+
+)
