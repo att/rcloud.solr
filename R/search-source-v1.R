7094dbc41ffa557d8cd8ff8ed909f1814c29d0fb
Doug Ashton <douglas.j.ashton@gmail.com>
2017-10-03 14:23:29 +0100

    further progress on v1 backwards compatibility #50

--- R/search-source-v1.R
+++ R/search-source-v1.R
@@ -31,3 +31,116 @@ SearchSourceV1 <- R6::R6Class(
 
 )
 
+
+#' Parse the result from a solr search
+#'
+#' @param solr.res The return value from the GET request
+#' @param pagesize solr pagesize
+#' @param source passed through from query
+#' @return The parsed search result
+#' @import rjson
+parse.solr.res <- function(solr.res, pagesize, source) {
+
+  if (!is.null(solr.res$error)) paste0(solr.res$error$code,": ",solr.res$error$msg)
+  response.docs <- solr.res$response$docs
+  response.high <- solr.res$highlighting
+
+  # Return error message if there is one
+  if(!is.null(solr.res$error)) {
+    return(c("error",solr.res$error$msg))
+  }
+
+  # Detect empty response
+  if(length(response.docs) <= 0) {
+    return(solr.res$response$docs)
+  }
+
+  # Process the highlighting
+  response.high <- lapply(response.high, parse.response.high)
+
+  # Build the output object
+  json <- create.json.output(response.docs, response.high, solr.res, pagesize, source)
+
+  return(json)
+}
+
+create.json.output <- function(response.docs, response.high, solr.res, pagesize, source) {
+
+  count <- solr.res$response$numFound
+  rows <- solr.res$params$rows
+
+
+  json <- list(
+    QTime = solr.res$responseHeader$QTime,
+    source = as.vector(source),
+    pagesize = pagesize,
+    n_notebooks = solr.res$response$numFound
+  )
+
+  docs <- lapply(response.docs, function(x) {
+    list(
+         notebook_id = x$id,
+         description = x$description,
+         starcount = x$starcount,
+         updated_at = x$updated_at,
+         user = x$user)
+    })
+
+  for(i in seq_along(docs)){
+    docs[[i]]$parts <- response.high[[i]]
+  }
+
+  json$docs <- docs
+
+  json
+}
+
+parse.response.high <- function(high) {
+
+  if(length(high) != 0) {
+    if(!is.null(high$content)) {
+      parts.content <- rjson::fromJSON(high$content)
+    } else {
+      high$content <- "[{\"filename\":\"part1.R\",\"content\":[]}]"
+      parts.content <- rjson::fromJSON(high$content)
+    }
+    if(!is.null(high$comments)) {
+      final_res <-list()
+      comments <- high$comments
+      for(n in 1: length(comments)) {
+        cmt_match <- grep("span_open_tag",comments[n])
+        if(as.logical(length(cmt_match))) {
+          final_res[[length(final_res)+1]] <- comments[n]
+        }
+      }
+      high$comments <- final_res
+      parts.content[[length(parts.content)+1]] <- list(filename="comments", content=high$comments)
+    }
+
+    # Only keep files with hits
+    just.contents <- vapply(parts.content, `[[`, "content", FUN.VALUE = character(1))
+    parts.content <- parts.content[grepl("span_open_tag", just.contents)]
+
+    # Easiest to loop round and swap the tags back
+    for(i in seq_along(parts.content)) {
+      parts.content[[i]]$content <- gsub(
+        "span_open_tag",
+        "<span class=\"search-result-solr-highlight\">",
+        parts.content[[i]]$content
+      )
+      parts.content[[i]]$content <-
+        gsub("span_close_tag", "</span>", parts.content[[i]]$content)
+
+      # Now trim the results down a bit
+      cont <-  parts.content[[i]]$content
+      i1 <- pmax(1, regexpr("<span ", cont) - 15)
+      i2 <- pmin(nchar(cont), regexpr("span>", parts.content[[i]]$content) + 20)
+      parts.content[[i]]$content <- substr(cont, i1, i2)
+    }
+
+
+  } else
+    parts.content <- list(file = "part1.R", content = "")
+
+  parts.content
+}
